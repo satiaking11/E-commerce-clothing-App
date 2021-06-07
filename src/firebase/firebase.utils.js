10015a38e06c4adfe397ce4f4bd6494d15dffcb9
satiaking11 <Santiatop01@gmail.com>
2021-06-07 14:45:47 -0400

    implements firebase utils, includes ability to store authenticated users

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,10 +12,38 @@ const config = {
     measurementId: "G-5ESWVF9KMM"
 };
 
-firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log("Erorr creating user: ", error.message);
+        }
+    }
+
+    return userRef;
+}
+
+!firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
 
 export const auth =  firebase.auth();
 export const firestore = firebase.firestore();
+
+
 
 //google authentication
 const provider = new firebase.auth.GoogleAuthProvider();
