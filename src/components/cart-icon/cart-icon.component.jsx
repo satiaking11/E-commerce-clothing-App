55da754bfe8e1ef3345495dbf62ad3598f680d2a
satiaking11 <Santiatop01@gmail.com>
2021-06-13 16:20:56 -0400

    updates app with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
+import {selectCartItems, selectCartItemsCount} from '../../redux/cart/cart.selectors';
 
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'; 
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 
@@ -18,4 +19,19 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+//this gets called, regardless if any items are being added, every time a state updates
+//this causes a lot of re-rendering, reduce always returns a new value even if no change
+//negative for performance
+//solution: store or cache the data so rerender doesn't happen, memoization + reselect
+/*const mapStateToProps = ({cart: {cartItems}}) => ({
+    //selector
+    itemCount: cartItems.reduce((accumalatedQuantity, cartItem) => 
+    accumalatedQuantity + cartItem.quantity, 0)
+})*/
+
+//passes whole reducer state into selector, which flows accordingly
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
