abc0187cdffc768dca819792932b091af089dbc0
satiaking11 <Santiatop01@gmail.com>
2021-06-13 22:14:43 -0400

    adds addItem adn removeItem functionality to checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,4 +1,4 @@
-export const addItemsToCart = (cartItems, cartItemToAdd) =>{
+export const addItemsToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(
         cartItem => cartItem.id === cartItemToAdd.id
     );
@@ -13,4 +13,23 @@ export const addItemsToCart = (cartItems, cartItemToAdd) =>{
     }
 
     return [...cartItems, {...cartItemToAdd, quantity: 1}]
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    )
+
+    if(existingCartItem.quantity === 1){
+        return cartItems.filter(
+            cartItem => cartItem.id !== cartItemToRemove.id
+        )
+    }
+
+    return cartItems.map(
+        cartItem =>
+            cartItem.id === cartItemToRemove.id ?
+            {...cartItem, quantity: cartItem.quantity - 1}
+            : cartItem
+    )
 }
